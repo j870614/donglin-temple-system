1693c7b5b1c584a72ffc3dd01b6257a154b5c383
Tuhacrt <a067800497@gmail.com>
2023-03-30 22:02:25 +0800

    chore: 更新 ESLint 設置，符合 Vue Typescript 環境 調整項目: 更新 .eslintrc.cjs ，導入 @vue/eslint-config-airbnb-with-typescript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,12 +7,14 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
+    '@vue/eslint-config-airbnb-with-typescript',
     '@vue/eslint-config-prettier/skip-formatting',
-    'airbnb-base',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
     ecmaVersion: 'latest',
   },
+  plugins: ['vue', 'prettier'],
   settings: {
     'import/resolver': {
       alias: {
