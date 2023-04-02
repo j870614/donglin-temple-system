f1d688b19fb571d869f5cad38d73fa20e5330029
panduola666 <panduola666@gmail.com>
2023-04-02 14:41:08 +0800

    style: 修改 sideBar 元件樣式
    調整項目:
    1. 刪除 logo 圖片
    2. 調整網格系統

--- src/components/back/SideBar.vue
+++ src/components/back/SideBar.vue
@@ -1,153 +1,149 @@
 <template>
-  <aside class="py-1 py-md-0" style="min-height: 100vh">
-    <img src="LOGO.png" alt="LOGO" v-if="isLogoShow" style="width: 100%" />
-    <div class="d-flex flex-column justify-content-between">
-      <ul class="list-inline" :style="{ marginTop: isLogoShow ? '2rem' : '' }">
-        <template v-for="nav in sideNav" :key="nav.name + nav.icon">
-          <!-- 第一層級 -->
-          <li
-            class="fs-5 style-sidebar flex-center"
-            :class="{
-              'bg-neutral-10':
-                hoverNavName === nav.name + nav.icon && currentNavName !== nav.name + nav.path,
-              'bg-primary text-white': !nav.children && currentNavName === nav.name + nav.path,
+  <div class="h-100 d-flex flex-column justify-content-between py-2 my-1 my-md-3 pt-md-0">
+    <ul class="list-inline">
+      <template v-for="nav in sideNav" :key="nav.name + nav.icon">
+        <!-- 第一層級 -->
+        <li
+          class="fs-5 style-sidebar flex-center"
+          :class="{
+            'bg-neutral-10':
+              hoverNavName === nav.name + nav.icon && currentNavName !== nav.name + nav.path,
+            'bg-primary text-white': !nav.children && currentNavName === nav.name + nav.path,
+          }"
+          @mouseenter="() => (hoverNavName = nav.name + nav.icon)"
+          @focus="() => (hoverNavName = nav.name + nav.icon)"
+          @mouseleave="() => (hoverNavName = '')"
+          @blur="() => (hoverNavName = '')"
+          @click="
+            () => {
+              nav.children ? changeOpen(nav, null) : changePath(nav.name, nav.path);
+            }
+          "
+          @keydown="
+            () => {
+              nav.children ? changeOpen(nav, null) : changePath(nav.name, nav.path);
+            }
+          "
+        >
+          <div class="fw-semibold d-flex align-items-center gap-3">
+            <span class="material-symbols-outlined fs-3">{{ nav.icon }} </span>
+            {{ nav.name }}
+          </div>
+          <span
+            v-if="nav.children"
+            class="material-symbols-outlined"
+            :style="{
+              transform: nav.isOpen ? 'rotate(180deg)' : 'rotate(0deg)',
             }"
-            @mouseenter="() => (hoverNavName = nav.name + nav.icon)"
-            @focus="() => (hoverNavName = nav.name + nav.icon)"
-            @mouseleave="() => (hoverNavName = '')"
-            @blur="() => (hoverNavName = '')"
-            @click="
-              () => {
-                nav.children ? changeOpen(nav, null) : changePath(nav.name, nav.path);
-              }
-            "
-            @keydown="
-              () => {
-                nav.children ? changeOpen(nav, null) : changePath(nav.name, nav.path);
-              }
-            "
           >
-            <div class="fw-semibold d-flex align-items-center gap-3">
-              <span class="material-symbols-outlined fs-3">{{ nav.icon }} </span>
-              {{ nav.name }}
-            </div>
-            <span
-              v-if="nav.children"
-              class="material-symbols-outlined"
-              :style="{
-                transform: nav.isOpen ? 'rotate(180deg)' : 'rotate(0deg)',
-              }"
-            >
-              expand_more
-            </span>
-          </li>
-          <template v-if="nav.children && nav.isOpen">
-            <template v-for="navChild in nav.children" :key="navChild.name + navChild.icon">
-              <template v-if="navChild.children && navChild.children.length">
-                <!-- 第二層級,有多層級 -->
-                <li
-                  class="fs-5 flex-center style-sidebar"
-                  :class="{
-                    'bg-neutral-10': hoverNavName === navChild.name + navChild.icon,
-                  }"
-                  @mouseenter="() => (hoverNavName = navChild.name + navChild.icon)"
-                  @focus="() => (hoverNavName = navChild.name + navChild.icon)"
-                  @mouseleave="() => (hoverNavName = '')"
-                  @blur="() => (hoverNavName = '')"
-                  @click="
-                    () => {
-                      changeOpen(nav, navChild);
-                    }
-                  "
-                  @keydown="
-                    () => {
-                      changeOpen(nav, navChild);
-                    }
-                  "
-                >
-                  <div class="fw-semibold d-flex align-items-center gap-3">
-                    <span class="material-symbols-outlined fs-3">{{ navChild.icon }} </span>
-                    {{ navChild.name }}
-                  </div>
-                  <span
-                    class="material-symbols-outlined"
-                    :style="{
-                      transform: navChild.isOpen ? 'rotate(180deg)' : 'rotate(0deg)',
-                    }"
-                  >
-                    expand_more
-                  </span>
-                </li>
-                <template v-if="navChild.isOpen">
-                  <ul
-                    class="list-inline border-start border-neutral-40 border-4"
-                    style="margin-left: 2rem"
-                    v-for="innerChild in navChild.children"
-                    :key="innerChild.name"
-                  >
-                    <!-- 第三層級 -->
-                    <li
-                      class="fs-5 style-sidebar"
-                      :class="{
-                        'bg-primary text-white':
-                          $route.fullPath.split('/').pop() === innerChild.path,
-                        'bg-neutral-10':
-                          hoverNavName === innerChild.name &&
-                          $route.fullPath.split('/').pop() !== innerChild.path,
-                      }"
-                      @mouseenter="() => (hoverNavName = innerChild.name)"
-                      @focus="() => (hoverNavName = innerChild.name)"
-                      @mouseleave="() => (hoverNavName = '')"
-                      @blur="() => (hoverNavName = '')"
-                      @click="() => changePath(innerChild.name, innerChild.path)"
-                      @keydown="() => changePath(innerChild.name, innerChild.path)"
-                    >
-                      <div class="fw-semibold">{{ innerChild.name }}</div>
-                    </li>
-                  </ul>
-                </template>
-              </template>
-              <!-- 僅第二層級 -->
+            expand_more
+          </span>
+        </li>
+        <template v-if="nav.children && nav.isOpen">
+          <template v-for="navChild in nav.children" :key="navChild.name + navChild.icon">
+            <template v-if="navChild.children && navChild.children.length">
+              <!-- 第二層級,有多層級 -->
               <li
-                v-else
-                class="fs-5 style-sidebar"
+                class="fs-5 flex-center style-sidebar"
                 :class="{
-                  'bg-primary text-white': $route.fullPath.split('/').pop() === navChild.path,
-                  'bg-neutral-10':
-                    hoverNavName === navChild.name && !$route.fullPath.includes(navChild.path),
+                  'bg-neutral-10': hoverNavName === navChild.name + navChild.icon,
                 }"
-                @mouseenter="() => (hoverNavName = navChild.name)"
-                @focus="() => (hoverNavName = navChild.name)"
+                @mouseenter="() => (hoverNavName = navChild.name + navChild.icon)"
+                @focus="() => (hoverNavName = navChild.name + navChild.icon)"
                 @mouseleave="() => (hoverNavName = '')"
                 @blur="() => (hoverNavName = '')"
-                @click="() => changePath(navChild.name, navChild.path)"
-                @keydown="() => changePath(navChild.name, navChild.path)"
+                @click="
+                  () => {
+                    changeOpen(nav, navChild);
+                  }
+                "
+                @keydown="
+                  () => {
+                    changeOpen(nav, navChild);
+                  }
+                "
               >
                 <div class="fw-semibold d-flex align-items-center gap-3">
                   <span class="material-symbols-outlined fs-3">{{ navChild.icon }} </span>
                   {{ navChild.name }}
                 </div>
+                <span
+                  class="material-symbols-outlined"
+                  :style="{
+                    transform: navChild.isOpen ? 'rotate(180deg)' : 'rotate(0deg)',
+                  }"
+                >
+                  expand_more
+                </span>
               </li>
+              <template v-if="navChild.isOpen">
+                <ul
+                  class="list-inline border-start border-neutral-40 border-4"
+                  style="margin-left: 2rem"
+                  v-for="innerChild in navChild.children"
+                  :key="innerChild.name"
+                >
+                  <!-- 第三層級 -->
+                  <li
+                    class="fs-5 style-sidebar"
+                    :class="{
+                      'bg-primary text-white': $route.fullPath.split('/').pop() === innerChild.path,
+                      'bg-neutral-10':
+                        hoverNavName === innerChild.name &&
+                        $route.fullPath.split('/').pop() !== innerChild.path,
+                    }"
+                    @mouseenter="() => (hoverNavName = innerChild.name)"
+                    @focus="() => (hoverNavName = innerChild.name)"
+                    @mouseleave="() => (hoverNavName = '')"
+                    @blur="() => (hoverNavName = '')"
+                    @click="() => changePath(innerChild.name, innerChild.path)"
+                    @keydown="() => changePath(innerChild.name, innerChild.path)"
+                  >
+                    <div class="fw-semibold">{{ innerChild.name }}</div>
+                  </li>
+                </ul>
+              </template>
             </template>
+            <!-- 僅第二層級 -->
+            <li
+              v-else
+              class="fs-5 style-sidebar"
+              :class="{
+                'bg-primary text-white': $route.fullPath.split('/').pop() === navChild.path,
+                'bg-neutral-10':
+                  hoverNavName === navChild.name && !$route.fullPath.includes(navChild.path),
+              }"
+              @mouseenter="() => (hoverNavName = navChild.name)"
+              @focus="() => (hoverNavName = navChild.name)"
+              @mouseleave="() => (hoverNavName = '')"
+              @blur="() => (hoverNavName = '')"
+              @click="() => changePath(navChild.name, navChild.path)"
+              @keydown="() => changePath(navChild.name, navChild.path)"
+            >
+              <div class="fw-semibold d-flex align-items-center gap-3">
+                <span class="material-symbols-outlined fs-3">{{ navChild.icon }} </span>
+                {{ navChild.name }}
+              </div>
+            </li>
           </template>
         </template>
-      </ul>
-      <router-link
-        to=""
-        class="fs-5 fw-semibold style-sidebar d-flex align-items-center gap-3"
-        :class="{ 'bg-neutral-10': hoverNavName === '登出' }"
-        @mouseenter="() => (hoverNavName = '登出')"
-        @focus="() => (hoverNavName = '登出')"
-        @mouseleave="() => (hoverNavName = '')"
-        @blur="() => (hoverNavName = '')"
-        ><span class="material-symbols-outlined"> logout </span>登出</router-link
-      >
-    </div>
-  </aside>
+      </template>
+    </ul>
+    <router-link
+      to=""
+      class="fs-5 fw-semibold style-sidebar d-flex align-items-center gap-3"
+      :class="{ 'bg-neutral-10': hoverNavName === '登出' }"
+      @mouseenter="() => (hoverNavName = '登出')"
+      @focus="() => (hoverNavName = '登出')"
+      @mouseleave="() => (hoverNavName = '')"
+      @blur="() => (hoverNavName = '')"
+      ><span class="material-symbols-outlined"> logout </span>登出</router-link
+    >
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref, defineProps, toRefs } from 'vue';
+import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 interface NavItem {
@@ -158,13 +154,6 @@ interface NavItem {
   children?: NavItem[];
 }
 
-const props = defineProps({
-  isLogoShow: {
-    type: Boolean,
-    default: true,
-  },
-});
-const { isLogoShow } = toRefs(props); // 控制當前是手機板 or 網頁版
 const router = useRouter();
 
 const nav = ref<NavItem[]>([
--- src/views/back/BackLayout.vue
+++ src/views/back/BackLayout.vue
@@ -9,16 +9,12 @@ const sideBarStore = sideBarConfigStore();
 <template>
   <div class="container-fluid">
     <div class="row">
-      <aside
-        class="col-12 col-md-3 gx-md-5 gy-md-4 gy-2"
-        style="max-width: 360px"
-        v-if="sideBarStore.isOpen"
-      >
+      <aside class="col-12 col-md-3 gx-md-5" v-if="sideBarStore.isOpen">
         <SideBar />
       </aside>
       <div class="col" style="background-color: #f6f6f6">
         <nav class="row">
-          <div class="col-12 bg-dark text-white">Nav</div>
+          <div class="bg-dark text-white">Nav</div>
         </nav>
         <RouterView />
       </div>
