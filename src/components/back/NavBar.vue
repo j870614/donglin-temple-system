968417363f9f6f4532bf7788f9e5610a01231457
IF <ast61006@gmail.com>
2023-04-04 17:46:48 +0800

    style:修改手機 logo 位置
    調整
    1.手機 logo 與寺務管理系統 ms-1 區隔

--- src/components/back/NavBar.vue
+++ src/components/back/NavBar.vue
@@ -30,7 +30,7 @@
     <div class="d-flex align-items-center">
       <img class="svg menu" src="@/assets/img/menu.svg" alt="menu" />
       <img class="logo-mobile ms-4" src="@/assets/img/Logo-mobile-sm.png" alt="Logo-mobile" />
-      <div class="h5 mb-0 logo-mobile-title">寺務管理系統</div>
+      <div class="h5 ms-1 mb-0 logo-mobile-title">寺務管理系統</div>
     </div>
   </div>
   <div class="col-4 d-flex d-md-none align-items-center d-flex justify-content-end">
