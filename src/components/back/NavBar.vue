01dba5c87619999aa78b0be818f4f8209ae36b9c
IF <ast61006@gmail.com>
2023-04-04 11:48:57 +0800

    feat:新增 NavBar 元件，純切版，沒JS控制sidebar
    1.調整 BackLayout.vue 使其載入NavBar 元件
    2.切手機板及桌機板
    3.圖片放入 src/assets/img/... 等
    4.icon 使用設計師給的 Material Design Icons
    
    問題:
    1.因使用設計師給的 Material Design Icons，我無法調整顏色
    2.icon 是否要統一來源?

---
+++ src/components/back/NavBar.vue
@@ -0,0 +1,108 @@
+<template>
+  <!-- 桌機 start -->
+  <div class="col-lg-6 d-none d-md-flex px-0">
+    <img class="logo px-4 mt-4" src="@/assets/img/LOGO.png" alt="spar-cat-eye" />
+    <div class="logo-title">寺務管理系統</div>
+  </div>
+  <div class="col-lg-6 d-none d-md-flex align-items-center d-flex justify-content-end">
+    <div class="me-18 icon-radius">
+      <img
+        class="svg bell-outline icon-position"
+        src="@/assets/img/bell-outline.svg"
+        alt="bell-outline"
+      />
+    </div>
+    <div class="icon-radius">
+      <img
+        class="svg account-circle icon-position"
+        src="@/assets/img/account-circle-outline.svg"
+        alt="account-circle"
+      />
+    </div>
+    <div class="login-name h5 py-5 mb-0">知客 普謀菩薩</div>
+    <div class="pe-4">
+      <img class="ic_ArrowDown svg" src="@/assets/img/chevron-down.svg" alt="ic_ArrowDown" />
+    </div>
+  </div>
+  <!-- 桌機 end -->
+  <!-- 手機 start -->
+  <div class="col-8 d-flex d-md-none">
+    <div class="d-flex align-items-center">
+      <img class="svg menu" src="@/assets/img/menu.svg" alt="menu" />
+      <img class="logo-mobile ms-4" src="@/assets/img/Logo-mobile.png" alt="Logo-mobile" />
+      <div class="h5 mb-0 logo-mobile-title">寺務管理系統</div>
+    </div>
+  </div>
+  <div class="col-4 d-flex d-md-none align-items-center d-flex justify-content-end">
+    <div class="me-2 icon-radius">
+      <img
+        class="svg bell-outline icon-position"
+        src="@/assets/img/bell-outline.svg"
+        alt="bell-outline"
+      />
+    </div>
+    <div class="icon-radius">
+      <img
+        class="svg account-circle icon-position"
+        src="@/assets/img/account-circle-outline.svg"
+        alt="account-circle"
+      />
+    </div>
+  </div>
+  <!-- 手機 end -->
+</template>
+<style scoped lang="scss">
+.logo {
+  width: 22.5rem;
+  height: 5rem;
+  &-title {
+    font-size: 2.5rem;
+    font-weight: 600;
+    line-height: 3rem;
+    padding-top: 2.25rem;
+    padding-bottom: 2.25rem;
+    padding-left: 1.25rem;
+    padding-right: 1.25rem;
+  }
+  &-mobile {
+    height: 2rem;
+    width: 1.25rem;
+    &-title {
+      font-weight: 600;
+      padding-top: 1.25rem;
+      padding-bottom: 1.25rem;
+    }
+  }
+}
+.icon-radius {
+  display: inline-block;
+  border-radius: 50%;
+  width: 44px;
+  height: 44px;
+  border: 1px solid #ececec;
+  position: relative;
+}
+.icon-position {
+  position: absolute;
+  left: 10px;
+  top: 10px;
+}
+
+/* ic_ArrowDown */
+
+.svg {
+  width: 1.5rem;
+  height: 1.5rem;
+}
+
+.login-name {
+  padding-left: 0.5rem;
+  padding-right: 1.125rem;
+  // font-size: 1.25rem;
+  line-height: 1.25rem;
+  font-weight: 600;
+}
+.me-18 {
+  margin-right: 1.125rem;
+}
+</style>
