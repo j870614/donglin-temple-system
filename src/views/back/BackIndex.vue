2436b3f3cddf4cb3141b60b2be10e36fd49bb011
panduola666 <panduola666@gmail.com>
2023-04-10 17:11:54 +0800

    style: 更改字體樣式 noto_sans

--- src/views/back/BackIndex.vue
+++ src/views/back/BackIndex.vue
@@ -5,22 +5,47 @@
         <OpenSideBar />彌陀之家東林寺常年佛七第422期
       </h1>
       <div class="row gy-3 gy-md-0">
-        <div class="col-md-4">
-          <div class="bg-black p-4">
-            <h2 class="h2 fw-semibold">彌陀之家東林寺公告</h2>
+        <div class="col-md-5">
+          <div class="box-style p-4">
+            <h2 class="h4 fw-semibold d-flex align-items-center justify-content-between mb-4 pb-2">
+              彌陀之家東林寺公告
+              <p
+                class="fs-6 fw-semibold d-flex align-items-center flex-shrink-0 mb-0"
+                style="cursor: pointer"
+              >
+                顯示更多
+                <span
+                  class="material-symbols-outlined fs-5 fw-semibold d-inline-block"
+                  data-v-1a886170=""
+                >
+                  arrow_forward
+                </span>
+              </p>
+            </h2>
+            <article class="d-flex align-items-center">
+              <div class="d-flex flex-column flex-grow-1">
+                <div class="mb-2">
+                  <span class="bg-primary-tint text-primary py-2 px-3 rounded-4 d-inline-block"
+                    >執事異動</span
+                  >
+                </div>
+                <p>普某師擔任知客師</p>
+              </div>
+              <div class="bg-neutral-40 p-2 rounded-4">8 月 31日</div>
+            </article>
           </div>
-          <div class="bg-danger p-4 mt-md-4 mt-3">
+          <div class="box-style p-4 mt-md-4 mt-3">
             <h2>系統公告</h2>
           </div>
         </div>
         <div class="col">
-          <div class="bg-info p-4">
+          <div class="box-style p-4">
             <h2 class="h2 fw-semibold">通知</h2>
           </div>
         </div>
       </div>
     </div>
-    <div class="col-md-3 bg-body">
+    <div class="col-md-3">
       <div class="mb-5 mb-md-0">
         <!-- 行事曆 -->
       </div>
@@ -32,3 +57,6 @@
 import OpenSideBar from '@/components/back/OpenSideBar.vue';
 import CalendarSub from '@/components/back/CalendarSub.vue';
 </script>
+<style scoped lang="scss">
+@import '@/assets/main.scss';
+</style>
