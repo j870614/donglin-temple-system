3804f7b4eefcacaf7b19ea36dc459fbae977f466
panduola666 <panduola666@gmail.com>
2023-04-10 19:08:01 +0800

    feat: 後台首頁主頁面消息補全
    調整項目:
    1. 因後續區塊有多個 圓角+陰影的區塊,並且有 hover 樣式,所以在 main.scss 添加了 box-style 和 box-hover
    2. 在文字大小 14px 新增 7 的級數
    3. 有看到其他組件圓角部分直接使用 rounded-4, 所以將其調整為 12px
    
    說明:
    1. 在公告 tag 上面使用 tags 變數來調整相對應的 class, 如後期虛心曾可在 tags 變數上增刪

---
+++ src/components/back/AnnouncementInfo.vue
@@ -0,0 +1,16 @@
+<template>
+  <article class="d-flex align-items-center" style="gap: 1rem">
+    <div class="d-flex flex-column flex-grow-1">
+      <div class="mb-2 d-flex align-items-center">
+        <slot name="info-tag"> 用行內塊做出 tag </slot>
+        <span class="fw-semibold ms-3">
+          <slot name="user"></slot>
+        </span>
+      </div>
+      <slot name="info-content">內容</slot>
+    </div>
+    <div class="bg-neutral-40 text-neutral-80 p-2 rounded-4" style="max-width: 55px">
+      <slot name="info-timer">日期</slot>
+    </div>
+  </article>
+</template>
