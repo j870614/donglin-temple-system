39343cfda1c914ac65c9177b13879ed525534824
panduola666 <panduola666@gmail.com>
2023-04-29 00:45:34 +0800

    feat: 使用者核發第二三步切版
    調整項目:
    1. 根據 url 的 step 來調整當前流程

--- src/views/back/AddAuthorization.vue
+++ src/views/back/AddAuthorization.vue
@@ -6,7 +6,80 @@
       </BackTitle>
       <ProcessSteps :steps="steps"></ProcessSteps>
       <div>
-        <beforeSearch></beforeSearch>
+        <beforeSearch v-if="Number($route.query.step) === 1" />
+        <template v-else-if="Number($route.query.step) === 2">
+          <section class="box-style p-lg-5 p-3">
+            <h2 class="h3 fw-semibold mb-4">請選擇堂口名稱及執事</h2>
+            <div class="row">
+              <div class="col-12 col-lg mb-lg-3 mb-4">
+                <label for="hall" class="form-label fw-semibold fs-5">堂口名稱</label>
+                <select
+                  name="堂口名稱"
+                  id="hall"
+                  class="form-select form-select-lg"
+                  aria-label=".form-select-lg example"
+                  v-model="currentHall"
+                >
+                  <option :value="index" v-for="(item, index) in auth" :key="index">
+                    {{ index }}
+                  </option>
+                </select>
+              </div>
+              <div class="col-12 col-lg">
+                <label for="deacon" class="form-label fw-semibold fs-5">執事名稱</label>
+                <select
+                  name="執事名稱"
+                  id="deacon"
+                  class="form-select form-select-lg"
+                  aria-label=".form-select-lg example"
+                >
+                  <option
+                    :value="item"
+                    v-for="(item, index) in auth[currentHall]"
+                    :key="item + index"
+                  >
+                    {{ item }}
+                  </option>
+                </select>
+              </div>
+            </div>
+          </section>
+          <div class="d-flex justify-content-end gap-3 mt-5">
+            <router-link
+              to="/back/permissions/authorized/new?step=1"
+              class="btn btn-outline-primary text-danger py-3 flex-grow-1"
+              style="max-width: 184px"
+            >
+              上一步
+            </router-link>
+            <router-link
+              to="/back/permissions/authorized/new?step=3"
+              class="btn btn-primary text-white py-3 flex-grow-1"
+              style="max-width: 184px"
+            >
+              產出註冊連結
+            </router-link>
+          </div>
+        </template>
+        <template v-else>
+          <div
+            class="d-flex flex-column flex-lg-row align-items-center justify-content-center gap-5"
+          >
+            <section>
+              <h2 class="h3 fw-semibold">使用者註冊連結</h2>
+              <div class="bg-secondary" style="width: 200px; height: 200px">這裡放QR</div>
+            </section>
+            <section class="text-danger">
+              <h3 class="h4 fw-semibold mb-3">注意事項</h3>
+              <p class="fs-5 mb-5">此為一次性註冊連結 <br />二十分鐘後失效</p>
+              <router-link
+                to="/back/permissions/authorized"
+                class="btn btn-lg btn-primary text-white"
+                >回權限核發頁</router-link
+              >
+            </section>
+          </div>
+        </template>
       </div>
     </div>
   </main>
@@ -17,6 +90,16 @@ import ProcessSteps from '@/components/back/ProcessSteps.vue';
 import beforeSearch from '@/components/information/beforeSearch.vue';
 
 import { ref } from 'vue';
+import type { Ref } from 'vue';
 
 const steps = ref(['個資查詢', '權限設定', '註冊連結']);
+
+interface Auth {
+  [key: string]: string[];
+}
+const auth = ref<Auth>({
+  知客: ['總知客', '副總知客', '知客志工'],
+  寮房: ['寮房師', '副寮房師', '寮房志工'],
+});
+const currentHall: Ref<string> = ref('知客');
 </script>
